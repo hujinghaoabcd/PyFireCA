92ceb6f2e32f6c32de3ef561bab2b3fb016844d5
Jinghao Hu <731409213@qq.com>
2026-08-12 20:02:15 +0800

    api: export verified standard fuel catalogue

--- src/pyfireca/behavior/__init__.py
+++ src/pyfireca/behavior/__init__.py
@@ -1,6 +1,12 @@
 """Wildfire behavior interfaces and reference implementations."""
 
 from pyfireca.behavior.base import FireBehaviorModel, FireBehaviorResult
+from pyfireca.behavior.fuel_catalog import (
+    StandardFuelModelRecord,
+    available_standard_fuel_model_numbers,
+    get_standard_fuel_model,
+    get_standard_fuel_model_record,
+)
 from pyfireca.behavior.rothermel import (
     FuelClass,
     FuelClassValues,
@@ -19,4 +25,8 @@ __all__ = [
     "RothermelFuelMoisture",
     "RothermelInputs",
     "RothermelModel",
+    "StandardFuelModelRecord",
+    "available_standard_fuel_model_numbers",
+    "get_standard_fuel_model",
+    "get_standard_fuel_model_record",
 ]
