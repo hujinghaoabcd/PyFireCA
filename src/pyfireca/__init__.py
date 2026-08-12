bffaff9a3161c533ba5ded8cd486d6e20083c173
Jinghao Hu <731409213@qq.com>
2026-08-12 20:13:56 +0800

    api: export physical arrival-time baseline

--- src/pyfireca/__init__.py
+++ src/pyfireca/__init__.py
@@ -1,5 +1,11 @@
 """PyFireCA: extensible cellular automata for wildfire spread research."""
 
+from pyfireca.arrival import (
+    ConstantDirectionalSpreadRate,
+    DirectionalSpreadRateProvider,
+    StaticArrivalTimeSolver,
+    arrival_times_to_state,
+)
 from pyfireca.data import LandscapeInput
 from pyfireca.grid import RasterGrid
 from pyfireca.neighborhood import MooreNeighborhood, VonNeumannNeighborhood
@@ -8,13 +14,17 @@ from pyfireca.simulation import Simulation
 from pyfireca.state import FireState
 
 __all__ = [
+    "ConstantDirectionalSpreadRate",
+    "DirectionalSpreadRateProvider",
     "FireState",
     "LandscapeInput",
     "MooreNeighborhood",
     "NeighborIgnitionRule",
     "RasterGrid",
     "Simulation",
+    "StaticArrivalTimeSolver",
     "VonNeumannNeighborhood",
+    "arrival_times_to_state",
 ]
 
 __version__ = "0.1.0a0"
