9ddc9b869ec23786f00ecd6c0dcb9719c5f9035b
Jinghao Hu <731409213@qq.com>
2026-08-13 00:17:12 +0800

    feat: export static simulator user API

--- src/pyfireca/__init__.py
+++ src/pyfireca/__init__.py
@@ -10,9 +10,15 @@ from pyfireca.behavior.rothermel_landscape import build_static_raster_rothermel_
 from pyfireca.data import LandscapeInput
 from pyfireca.edge_coupling import HalfCellInterfaceDirectionalSpreadRate
 from pyfireca.grid import RasterGrid
+from pyfireca.ignition import IgnitionEvent, build_ignition_times
 from pyfireca.neighborhood import MooreNeighborhood, VonNeumannNeighborhood
 from pyfireca.rules import NeighborIgnitionRule
 from pyfireca.simulation import Simulation
+from pyfireca.simulator import (
+    StaticWildfireSimulationRequest,
+    StaticWildfireSimulationResult,
+    run_static_wildfire_simulation,
+)
 from pyfireca.state import FireState
 
 __all__ = [
@@ -20,15 +26,20 @@ __all__ = [
     "DirectionalSpreadRateProvider",
     "FireState",
     "HalfCellInterfaceDirectionalSpreadRate",
+    "IgnitionEvent",
     "LandscapeInput",
     "MooreNeighborhood",
     "NeighborIgnitionRule",
     "RasterGrid",
     "Simulation",
     "StaticArrivalTimeSolver",
+    "StaticWildfireSimulationRequest",
+    "StaticWildfireSimulationResult",
     "VonNeumannNeighborhood",
     "arrival_times_to_state",
+    "build_ignition_times",
     "build_static_raster_rothermel_arrival_solver",
+    "run_static_wildfire_simulation",
 ]
 
 __version__ = "0.1.0a0"
