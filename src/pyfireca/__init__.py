9e14d8959bd9a4ab2e39e44410e72a62cdaf7cd8
Jinghao Hu <731409213@qq.com>
2026-08-13 00:42:18 +0800

    feat: export stable baseline run workflow API

--- src/pyfireca/__init__.py
+++ src/pyfireca/__init__.py
@@ -7,6 +7,7 @@ from pyfireca.arrival import (
     arrival_times_to_state,
 )
 from pyfireca.behavior.rothermel_landscape import build_static_raster_rothermel_arrival_solver
+from pyfireca.config import StaticRasterInputPaths, StaticRunConfig, load_static_run_config
 from pyfireca.data import LandscapeInput
 from pyfireca.edge_coupling import HalfCellInterfaceDirectionalSpreadRate
 from pyfireca.grid import RasterGrid
@@ -15,6 +16,7 @@ from pyfireca.neighborhood import MooreNeighborhood, VonNeumannNeighborhood
 from pyfireca.outputs import (
     StaticSimulationOutputPaths,
     terminal_state_from_result,
+    write_burned_perimeter_geojson,
     write_static_simulation_outputs,
 )
 from pyfireca.rules import NeighborIgnitionRule
@@ -25,6 +27,7 @@ from pyfireca.simulator import (
     run_static_wildfire_simulation,
 )
 from pyfireca.state import FireState
+from pyfireca.workflow import StaticRunArtifacts, run_static_config, validate_static_run
 
 __all__ = [
     "ConstantDirectionalSpreadRate",
@@ -38,6 +41,9 @@ __all__ = [
     "RasterGrid",
     "Simulation",
     "StaticArrivalTimeSolver",
+    "StaticRasterInputPaths",
+    "StaticRunArtifacts",
+    "StaticRunConfig",
     "StaticSimulationOutputPaths",
     "StaticWildfireSimulationRequest",
     "StaticWildfireSimulationResult",
@@ -45,8 +51,12 @@ __all__ = [
     "arrival_times_to_state",
     "build_ignition_times",
     "build_static_raster_rothermel_arrival_solver",
+    "load_static_run_config",
+    "run_static_config",
     "run_static_wildfire_simulation",
     "terminal_state_from_result",
+    "validate_static_run",
+    "write_burned_perimeter_geojson",
     "write_static_simulation_outputs",
 ]
 
