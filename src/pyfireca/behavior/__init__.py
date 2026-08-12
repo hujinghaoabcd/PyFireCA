e87565aebfeb062e68d3e0fb6c3ad59500791fac
Jinghao Hu <731409213@qq.com>
2026-08-12 10:58:44 +0800

    api: expose Rothermel input contracts

--- src/pyfireca/behavior/__init__.py
+++ src/pyfireca/behavior/__init__.py
@@ -1,5 +1,20 @@
 """Wildfire behavior interfaces and reference implementations."""
 
 from pyfireca.behavior.base import FireBehaviorModel, FireBehaviorResult
+from pyfireca.behavior.rothermel import (
+    FuelClass,
+    FuelClassValues,
+    RothermelFuelModel,
+    RothermelFuelMoisture,
+    RothermelInputs,
+)
 
-__all__ = ["FireBehaviorModel", "FireBehaviorResult"]
+__all__ = [
+    "FireBehaviorModel",
+    "FireBehaviorResult",
+    "FuelClass",
+    "FuelClassValues",
+    "RothermelFuelModel",
+    "RothermelFuelMoisture",
+    "RothermelInputs",
+]
