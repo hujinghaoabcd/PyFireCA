d806931851d8048492cb3b7620051d5b80cd31cb
Jinghao Hu <731409213@qq.com>
2026-08-12 10:27:11 +0800

    api: expose minimal CA reference components

--- src/pyfireca/__init__.py
+++ src/pyfireca/__init__.py
@@ -1,8 +1,18 @@
 """PyFireCA: extensible cellular automata for wildfire spread research."""
 
+from pyfireca.grid import RasterGrid
 from pyfireca.neighborhood import MooreNeighborhood, VonNeumannNeighborhood
+from pyfireca.rules import NeighborIgnitionRule
+from pyfireca.simulation import Simulation
 from pyfireca.state import FireState
 
-__all__ = ["FireState", "MooreNeighborhood", "VonNeumannNeighborhood"]
+__all__ = [
+    "FireState",
+    "MooreNeighborhood",
+    "NeighborIgnitionRule",
+    "RasterGrid",
+    "Simulation",
+    "VonNeumannNeighborhood",
+]
 
 __version__ = "0.1.0a0"
